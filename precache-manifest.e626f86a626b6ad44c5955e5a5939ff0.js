2a36814c6960123dca8de1b442ea27ca66bcbfee
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-12-18 09:51:53 +0100

    Updates

---
+++ precache-manifest.e626f86a626b6ad44c5955e5a5939ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53db7aa46186e864059012cde809d48",
+    "url": "/.github.io/movie-app-react-redux-router/index.html"
+  },
+  {
+    "revision": "7248b784adfaec07f51a",
+    "url": "/.github.io/movie-app-react-redux-router/static/css/main.3ba39aa0.chunk.css"
+  },
+  {
+    "revision": "bfa9c2bfc6e1ce7ba89d",
+    "url": "/.github.io/movie-app-react-redux-router/static/js/2.b3fcd267.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/.github.io/movie-app-react-redux-router/static/js/2.b3fcd267.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7248b784adfaec07f51a",
+    "url": "/.github.io/movie-app-react-redux-router/static/js/main.aa83310e.chunk.js"
+  },
+  {
+    "revision": "15347eb8537f6e4c44e6",
+    "url": "/.github.io/movie-app-react-redux-router/static/js/runtime-main.8bab6ed2.js"
+  }
+]);
